5da09c3046a399f65c17fe96fdc767d62e2f3313
Javone <javosmith12@gmail.com>
2020-05-11 17:14:58 -0400

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(input)
+  number = input.to_i
+  number - 1
+end
+
+def move(board,position,char="X")
+  # char = chomp.strip
+   board[position] = char
+end
